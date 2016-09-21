7e05cbb484de2cfb4e35da859cd7cd8581e78bea
santhosh <santhosh.prabhu01@infosys.com>
2016-09-22 01:36:27 +0530

    adding a apache server

---
+++ signature/apache/override.py
@@ -0,0 +1,4 @@
+from signature.settings import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['www.mydomain.com', 'mydomain.com']
---
+++ signature/apache/wsgi.py
@@ -0,0 +1,13 @@
+import os, sys
+from django.core.wsgi import get_wsgi_application
+# Calculate the path based on the location of the WSGI script.
+apache_configuration= os.path.dirname(__file__)
+project = os.path.dirname(apache_configuration)
+workspace = os.path.dirname(project)
+sys.path.append(workspace)
+sys.path.append(project)
+
+# Add the path to 3rd party django application and to django itself.
+sys.path.append('/home/santhosh')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'signature.apache.override'
+application = get_wsgi_application()
